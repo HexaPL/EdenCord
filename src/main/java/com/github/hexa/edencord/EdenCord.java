4ca2bf732b247099cd0d2992fba3370bb0d46f1d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-27 17:34:43 +0200

    Register status command

--- src/main/java/com/github/hexa/edencord/EdenCord.java
+++ src/main/java/com/github/hexa/edencord/EdenCord.java
@@ -1,11 +1,20 @@
 package com.github.hexa.edencord;
 
+import net.essentialsx.api.v2.services.discord.DiscordService;
+import net.essentialsx.api.v2.services.discord.InteractionException;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EdenCord extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("EdenCord enabled!");
+        final DiscordService api = Bukkit.getServicesManager().load(DiscordService.class);
+        try {
+            api.getInteractionController().registerCommand(new StatusCommand());
+        } catch (InteractionException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
